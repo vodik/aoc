c8f111f49a01328390280bf009dde25e6febbd35
Simon Gomizelj <simon@vodik.xyz>
2020-12-13 18:59:45 -0500

    Solve day9 puzzle

---
+++ src/day9.rs
@@ -0,0 +1,62 @@
+use std::cmp::Ordering;
+use std::num::ParseIntError;
+
+const PREAMBLE_LEN: usize = 25;
+
+#[aoc_generator(day9)]
+fn parse_input(input: &str) -> Result<Vec<u64>, ParseIntError> {
+    input.lines().map(str::parse).collect()
+}
+
+fn tails<T>(data: &[T]) -> impl Iterator<Item = (&T, &[T])> + '_ {
+    data.iter()
+        .enumerate()
+        .map(move |(idx, v)| (v, &data[idx + 1..]))
+}
+
+fn slices<T>(data: &[T]) -> impl Iterator<Item = &[T]> + '_ {
+    data.iter().enumerate().map(move |(idx, _)| &data[idx..])
+}
+
+#[aoc(day9, part1)]
+fn part1(data: &[u64]) -> Option<u64> {
+    data.windows(PREAMBLE_LEN + 1).find_map(|window| {
+        let target = window[PREAMBLE_LEN];
+
+        if tails(&window[..PREAMBLE_LEN])
+            .flat_map(|(x, xs)| xs.iter().map(move |y| x + y))
+            .any(|sum| sum == target)
+        {
+            None
+        } else {
+            Some(target)
+        }
+    })
+}
+
+#[aoc(day9, part2)]
+fn part2(data: &[u64]) -> Option<u64> {
+    let weakness = part1(data)?;
+
+    slices(data)
+        .find_map(|slice| {
+            let mut sum = 0;
+
+            for (idx, x) in slice.iter().enumerate() {
+                sum += x;
+
+                match sum.cmp(&weakness) {
+                    Ordering::Greater => break,
+                    Ordering::Equal => return Some(&slice[..idx]),
+                    _ => {}
+                }
+            }
+
+            None
+        })
+        .and_then(|slice| {
+            let min = slice.iter().min()?;
+            let max = slice.iter().max()?;
+            Some(min + max)
+        })
+}
