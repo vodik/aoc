ca20bcca40a5895c65dc9558190ddaec31440478
Simon Gomizelj <simon@vodik.xyz>
2021-12-02 02:36:28 -0500

    Exploit mathematical properties of day1
    Instead of making a intermediate vector of sums, we can exploit that
    its effectively `A + B + C > B + C + D`.
    
    That in turn reduces to just `A + D`.

--- src/day01.rs
+++ src/day01.rs
@@ -2,17 +2,16 @@ pub fn parse_input(input: &str) -> Vec<u32> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-fn decent_step_count(data: &[u32]) -> usize {
-    data.windows(2)
-        .filter(|window| window[0] < window[1])
+fn decent_step_count(size: usize, data: &[u32]) -> usize {
+    data.windows(size)
+        .filter(|window| window[0] < window[size - 1])
         .count()
 }
 
 pub fn part1(input: &[u32]) -> usize {
-    decent_step_count(input)
+    decent_step_count(2, input)
 }
 
 pub fn part2(input: &[u32]) -> usize {
-    let windows: Vec<u32> = input.windows(3).map(|window| window.iter().sum()).collect();
-    decent_step_count(&windows)
+    decent_step_count(4, input)
 }
