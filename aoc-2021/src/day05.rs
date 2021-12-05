39d51d93e5f242cf4bf97b90803fa83ac742be8d
Simon Gomizelj <simon@vodik.xyz>
2021-12-05 01:51:34 -0500

    Optimize day5 by using a flat array to store points

--- aoc-2021/src/day05.rs
+++ aoc-2021/src/day05.rs
@@ -7,9 +7,9 @@ use nom::{
     sequence::{separated_pair, terminated},
     Finish, IResult,
 };
-use std::{collections::HashMap, ops::RangeInclusive, str::FromStr};
+use std::{ops::RangeInclusive, str::FromStr};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug)]
 pub struct Point {
     x: i32,
     y: i32,
@@ -21,50 +21,53 @@ impl Point {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub struct Segment(Point, Point);
 
 impl Segment {
+    fn new(a: Point, b: Point) -> Self {
+        Segment(a, b)
+    }
+
     fn is_orthogonal(&self) -> bool {
         self.0.x == self.1.x || self.0.y == self.1.y
     }
 
-    fn points(&self) -> SegmentIter {
+    fn interpolate(&self) -> SegmentIter {
         if self.0.x == self.1.x {
             let x = self.0.x;
             let y0 = i32::min(self.0.y, self.1.y);
             let y1 = i32::max(self.0.y, self.1.y);
 
-            SegmentIter::Vertical { y: (y0..=y1), x }
+            SegmentIter::Vertical { y_iter: (y0..=y1), x }
         } else if self.0.y == self.1.y {
+            let x0 = i32::min(self.0.x, self.1.x);
+            let x1 = i32::max(self.0.x, self.1.x);
             let y = self.0.y;
-            let x0 = i32::min(self.0.y, self.1.y);
-            let x1 = i32::max(self.0.y, self.1.y);
 
-            SegmentIter::Horizontal { x: (x0..=x1), y }
+            SegmentIter::Horizontal { x_iter: (x0..=x1), y }
         } else {
+            let x0 = i32::min(self.0.x, self.1.x);
+            let x1 = i32::max(self.0.x, self.1.x);
             let m = (self.1.y - self.0.y) / (self.1.x - self.0.x);
             let b = self.0.y - m * self.0.x;
 
-            let x0 = i32::min(self.0.x, self.1.x);
-            let x1 = i32::max(self.0.x, self.1.x);
-
-            SegmentIter::Line { x: (x0..=x1), m, b }
+            SegmentIter::Sloped { x_iter: (x0..=x1), m, b }
         }
     }
 }
 
 enum SegmentIter {
-    Horizontal {
-        x: RangeInclusive<i32>,
-        y: i32,
-    },
     Vertical {
         x: i32,
-        y: RangeInclusive<i32>,
+        y_iter: RangeInclusive<i32>,
     },
-    Line {
-        x: RangeInclusive<i32>,
+    Horizontal {
+        x_iter: RangeInclusive<i32>,
+        y: i32,
+    },
+    Sloped {
+        x_iter: RangeInclusive<i32>,
         m: i32,
         b: i32,
     },
@@ -74,18 +77,18 @@ impl Iterator for SegmentIter {
     type Item = Point;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self {
-            SegmentIter::Horizontal { x, y } => {
-                let x = x.next()?;
-                Some(Point::new(x, *y))
+        match *self {
+            SegmentIter::Vertical { x, ref mut y_iter } => {
+                let y = y_iter.next()?;
+                Some(Point::new(x, y))
             }
-            SegmentIter::Vertical { x, y } => {
-                let y = y.next()?;
-                Some(Point::new(*x, y))
+            SegmentIter::Horizontal { ref mut x_iter, y } => {
+                let x = x_iter.next()?;
+                Some(Point::new(x, y))
             }
-            SegmentIter::Line { x, m, b } => {
-                let x = x.next()?;
-                let y = x * *m + *b;
+            SegmentIter::Sloped { ref mut x_iter, m, b } => {
+                let x = x_iter.next()?;
+                let y = x * m + b;
                 Some(Point::new(x, y))
             }
         }
@@ -105,7 +108,7 @@ fn parse_point(input: &str) -> IResult<&str, Point> {
 fn parse_line(input: &str) -> IResult<&str, Segment> {
     map(
         separated_pair(parse_point, tag(" -> "), parse_point),
-        |(a, b)| Segment(a, b),
+        |(a, b)| Segment::new(a, b),
     )(input)
 }
 
@@ -124,29 +127,32 @@ pub fn parse_input(input: &str) -> Vec<Segment> {
     .unwrap()
 }
 
-pub fn part1(input: &[Segment]) -> usize {
-    let points = input
-        .iter()
-        .filter(|&segment| segment.is_orthogonal())
-        .flat_map(|segment| segment.points());
+fn count_points_of_interest(points: impl Iterator<Item = Point>) -> usize {
+    let mut map = [0u8; 1000 * 1000];
+    let mut points_of_interest = 0;
 
-    let mut map: HashMap<Point, usize> = HashMap::new();
-    for point in points {
-        let entry = map.entry(point).or_default();
-        *entry += 1
+    for Point { x, y } in points {
+        let index = x as usize * 1000 + y as usize;
+        let count = &mut map[index];
+
+        *count = count.saturating_add(1);
+        if *count == 2 {
+            points_of_interest += 1;
+        }
     }
 
-    map.into_values().filter(|&count| count > 1).count()
+    points_of_interest
+}
+
+pub fn part1(input: &[Segment]) -> usize {
+    count_points_of_interest(
+        input
+            .iter()
+            .filter(|segment| segment.is_orthogonal())
+            .flat_map(|segment| segment.interpolate()),
+    )
 }
 
 pub fn part2(input: &[Segment]) -> usize {
-    let points = input.iter().flat_map(|segment| segment.points());
-
-    let mut map: HashMap<Point, usize> = HashMap::new();
-    for point in points {
-        let entry = map.entry(point).or_default();
-        *entry += 1
-    }
-
-    map.into_values().filter(|&count| count > 1).count()
+    count_points_of_interest(input.iter().flat_map(|segment| segment.interpolate()))
 }
