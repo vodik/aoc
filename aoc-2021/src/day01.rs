c9a9bd73c58e6978eac3e129bff64f03850d08b4
Simon Gomizelj <simon@vodik.xyz>
2021-12-04 21:53:25 -0500

    Optimize day1 with const generics
    Performs slightly faster for reasons not clear to me

--- aoc-2021/src/day01.rs
+++ aoc-2021/src/day01.rs
@@ -2,16 +2,16 @@ pub fn parse_input(input: &str) -> Vec<u32> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-fn decent_step_count(size: usize, data: &[u32]) -> usize {
-    data.windows(size)
-        .filter(|&window| window[0] < window[size - 1])
+fn decent_step_count<const N: usize>(data: &[u32]) -> usize {
+    data.windows(N)
+        .filter(|&window| window[0] < window[N - 1])
         .count()
 }
 
 pub fn part1(input: &[u32]) -> usize {
-    decent_step_count(2, input)
+    decent_step_count::<2>(input)
 }
 
 pub fn part2(input: &[u32]) -> usize {
-    decent_step_count(4, input)
+    decent_step_count::<4>(input)
 }
