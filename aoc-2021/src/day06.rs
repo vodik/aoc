d776d61f169923f202c05ced675e6338dab8b788
Simon Gomizelj <simon@vodik.xyz>
2021-12-06 03:00:06 -0500

    Optimize day6 by manually unrolling the loop

--- aoc-2021/src/day06.rs
+++ aoc-2021/src/day06.rs
@@ -7,17 +7,28 @@ pub fn parse_input(input: &str) -> Vec<u8> {
 }
 
 fn simulate(data: &[u8], generations: usize) -> usize {
-    let mut tank = [0usize; 9];
+    let mut tank = [0; 9];
     for &fish in data {
         tank[fish as usize] += 1;
     }
 
-    for generation in 0..generations {
-        let base = generation % 9;
-        tank[(base + 7) % 9] += tank[base];
+    for _ in 0..(generations / 9) {
+        tank[7] += tank[0];
+        tank[8] += tank[1];
+        tank[0] += tank[2];
+        tank[1] += tank[3];
+        tank[2] += tank[4];
+        tank[3] += tank[5];
+        tank[4] += tank[6];
+        tank[5] += tank[7];
+        tank[6] += tank[8];
     }
 
-    tank.into_iter().sum()
+    for generation in 0..(generations % 9) {
+        tank[(generation + 7) % 9] += tank[generation];
+    }
+
+    tank.iter().sum()
 }
 
 pub fn part1(input: &[u8]) -> usize {
