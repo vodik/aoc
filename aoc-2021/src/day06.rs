3ab89021c6f9f39510b6ff504a3638a2a783e315
Simon Gomizelj <simon@vodik.xyz>
2021-12-06 01:25:13 -0500

    Optimize day6 solution

--- aoc-2021/src/day06.rs
+++ aoc-2021/src/day06.rs
@@ -1,4 +1,4 @@
-pub fn parse_input(input: &str) -> Vec<u64> {
+pub fn parse_input(input: &str) -> Vec<u8> {
     input
         .lines()
         .flat_map(|line| line.split(','))
@@ -6,24 +6,24 @@ pub fn parse_input(input: &str) -> Vec<u64> {
         .collect()
 }
 
-fn simulate(data: &[u64], generations: usize) -> usize {
+fn simulate(data: &[u8], generations: usize) -> usize {
     let mut tank = [0usize; 9];
     for &fish in data {
         tank[fish as usize] += 1;
     }
 
-    for _ in 0..generations {
-        tank.rotate_left(1);
-        tank[6] += tank[8];
+    for generation in 0..generations {
+        let base = generation % 9;
+        tank[(base + 7) % 9] += tank[base];
     }
 
     tank.into_iter().sum()
 }
 
-pub fn part1(input: &[u64]) -> usize {
+pub fn part1(input: &[u8]) -> usize {
     simulate(input, 80)
 }
 
-pub fn part2(input: &[u64]) -> usize {
+pub fn part2(input: &[u8]) -> usize {
     simulate(input, 256)
 }
