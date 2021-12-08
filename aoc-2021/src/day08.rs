6bc52a43f61119e57df93d351abd63d9677e4c0e
Simon Gomizelj <simon@vodik.xyz>
2021-12-08 08:55:09 -0500

    Optimize day8's map building

--- aoc-2021/src/day08.rs
+++ aoc-2021/src/day08.rs
@@ -1,14 +1,12 @@
+type Pattern = u8;
+
 #[derive(Debug)]
 pub struct Entry {
-    signal: Vec<Segments>,
-    output: Vec<Segments>,
+    signal: Vec<Pattern>,
+    output: Vec<Pattern>,
 }
 
-// #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-type Segments = u8;
-// pub struct Segment(u8);
-
-fn parse2(input: &str) -> Vec<Segments> {
+fn parse_segments(input: &str) -> Vec<Pattern> {
     input
         .split(' ')
         .map(|segment| {
@@ -37,8 +35,8 @@ pub fn parse_input(input: &str) -> Vec<Entry> {
         .map(|line| {
             let (signal, output) = line.split_once(" | ").unwrap();
             Entry {
-                signal: parse2(signal),
-                output: parse2(output),
+                signal: parse_segments(signal),
+                output: parse_segments(output),
             }
         })
         .collect()
@@ -48,68 +46,56 @@ pub fn part1(input: &[Entry]) -> usize {
     input
         .iter()
         .flat_map(|entry| &entry.output)
-        .filter(|&segment| matches!(segment.count_ones(), 2 | 3 | 4 | 7))
+        .filter(|&pattern| matches!(pattern.count_ones(), 2 | 3 | 4 | 7))
         .count()
 }
 
-pub fn map_digits(mut possibilities: Vec<Segments>) -> [Segments; 10] {
-    let mut known = [0; 10];
+pub fn map_digits(possibilities: &[Pattern]) -> [u8; 1 << 7] {
+    let mut one = 0;
+    let mut four = 0;
 
-    for (pos, count) in [(1, 2), (7, 3), (4, 4), (8, 7)] {
-        known[pos] = possibilities.remove(
-            possibilities
-                .iter()
-                .position(|x| x.count_ones() == count)
-                .unwrap(),
-        );
+    for &pattern in possibilities {
+        match pattern.count_ones() {
+            2 => {
+                one = pattern;
+            }
+            4 => {
+                four = pattern;
+            }
+            _ => {}
+        }
     }
 
-    known[6] = possibilities.remove(
-        possibilities
-            .iter()
-            .position(|x| x.count_ones() == 6 && (x & !known[1]).count_ones() == 5)
-            .unwrap(),
-    );
+    let mut map = [0; 1 << 7];
+    map[one as usize] = 1;
+    map[four as usize] = 4;
 
-    known[0] = possibilities.remove(
-        possibilities
-            .iter()
-            .position(|x| x.count_ones() == 6 && (x & !known[4]).count_ones() == 3)
-            .unwrap(),
-    );
+    for &pattern in possibilities {
+        let digit = match pattern.count_ones() {
+            2 | 4 => continue,
+            3 => 7,
+            6 if (pattern & !one).count_ones() == 5 => 6,
+            6 if (pattern & !four).count_ones() == 3 => 0,
+            6 => 9,
+            7 => 8,
+            _ if (pattern & one).count_ones() == 2 => 3,
+            _ if (pattern & four).count_ones() == 2 => 2,
+            _ => 5,
+        };
 
-    known[9] = possibilities.remove(
-        possibilities
-            .iter()
-            .position(|x| x.count_ones() == 6)
-            .unwrap(),
-    );
+        map[pattern as usize] = digit;
+    }
 
-    known[2] = possibilities.remove(
-        possibilities
-            .iter()
-            .position(|e| (e & known[4]).count_ones() == 2)
-            .unwrap(),
-    );
-
-    known[3] = possibilities.remove(
-        possibilities
-            .iter()
-            .position(|e| (e & known[1]).count_ones() == 2)
-            .unwrap(),
-    );
-
-    known[5] = possibilities.pop().unwrap();
-    known
+    map
 }
 
 pub fn decode(entry: &Entry) -> u32 {
-    let map = map_digits(entry.signal.clone());
+    let map = map_digits(&entry.signal);
 
     entry
         .output
         .iter()
-        .map(|&x| map.iter().position(|&y| x == y).unwrap() as u32)
+        .map(|&x| map[x as usize] as u32)
         .fold(0, |acc, value| acc * 10 + value)
 }
 
