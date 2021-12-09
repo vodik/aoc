ff5e5312e6536595fe6252f1ee42880baf371b31
Simon Gomizelj <simon@vodik.xyz>
2021-12-08 19:53:39 -0500

    Refactor day8

--- aoc-2021/src/day08.rs
+++ aoc-2021/src/day08.rs
@@ -9,7 +9,7 @@ pub struct Entry {
 fn parse_segments(input: &str) -> Vec<Pattern> {
     input
         .split(' ')
-        .flat_map(|segment| {
+        .map(|segment| {
             segment
                 .as_bytes()
                 .iter()
@@ -23,7 +23,7 @@ fn parse_segments(input: &str) -> Vec<Pattern> {
                     b'g' => 1 << 6,
                     _ => unreachable!(),
                 })
-                .reduce(|acc, bit| acc | bit)
+                .sum()
         })
         .collect()
 }
@@ -49,50 +49,58 @@ pub fn part1(input: &[Entry]) -> usize {
         .count()
 }
 
-fn map_digits(possibilities: &[Pattern]) -> [u8; 1 << 7] {
-    let mut one = 0;
-    let mut four = 0;
+struct DecipherKey([u8; 1 << 7]);
 
-    for &pattern in possibilities {
-        match pattern.count_ones() {
-            2 => {
-                one = pattern;
+impl DecipherKey {
+    fn from(possibilities: &[Pattern]) -> Self {
+        let mut one = 0;
+        let mut four = 0;
+
+        for &pattern in possibilities {
+            match pattern.count_ones() {
+                2 => {
+                    one = pattern;
+                }
+                4 => {
+                    four = pattern;
+                }
+                _ => {}
             }
-            4 => {
-                four = pattern;
-            }
-            _ => {}
         }
+
+        let mut map = [0; 1 << 7];
+        for &pattern in possibilities {
+            map[pattern as usize] = match pattern.count_ones() {
+                2 => 1,
+                3 => 7,
+                4 => 4,
+                5 if (pattern & one).count_ones() == 2 => 3,
+                5 if (pattern & four).count_ones() == 2 => 2,
+                5 => 5,
+                6 if (pattern & !one).count_ones() == 5 => 6,
+                6 if (pattern & !four).count_ones() == 3 => 0,
+                6 => 9,
+                7 => 8,
+                _ => unreachable!(),
+            };
+        }
+        Self(map)
     }
 
-    let mut map = [0; 1 << 7];
-    for &pattern in possibilities {
-        map[pattern as usize] = match pattern.count_ones() {
-            2 => 1,
-            3 => 7,
-            4 => 4,
-            6 if (pattern & !one).count_ones() == 5 => 6,
-            6 if (pattern & !four).count_ones() == 3 => 0,
-            6 => 9,
-            7 => 8,
-            _ if (pattern & one).count_ones() == 2 => 3,
-            _ if (pattern & four).count_ones() == 2 => 2,
-            _ => 5,
-        };
+    fn decode(&self, pattern: Pattern) -> u8 {
+        self.0[pattern as usize]
     }
-    map
-}
-
-fn decode(entry: &Entry) -> u32 {
-    let map = map_digits(&entry.signal);
-
-    entry
-        .output
-        .iter()
-        .map(|&x| map[x as usize] as u32)
-        .fold(0, |acc, value| acc * 10 + value)
 }
 
 pub fn part2(input: &[Entry]) -> u32 {
-    input.iter().map(decode).sum()
+    input
+        .iter()
+        .map(|Entry { signal, output }| {
+            let key = DecipherKey::from(signal);
+            output
+                .iter()
+                .map(|&x| key.decode(x) as u32)
+                .fold(0, |acc, value| acc * 10 + value)
+        })
+        .sum()
 }
