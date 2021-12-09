a128bb3aac9a7aee5c6a77dd7a369176ccdf6a31
Simon Gomizelj <simon@vodik.xyz>
2021-12-09 04:15:59 -0500

    Optimize day8 with frequency analysis

--- aoc-2021/src/day08.rs
+++ aoc-2021/src/day08.rs
@@ -49,46 +49,25 @@ pub fn part1(input: &[Entry]) -> usize {
         .count()
 }
 
-struct DecipherKey([u8; 1 << 7]);
+struct DecipherKey(Pattern, Pattern);
 
 impl DecipherKey {
-    fn from(possibilities: &[Pattern]) -> Self {
-        let mut one = 0;
-        let mut four = 0;
-
-        for &pattern in possibilities {
-            match pattern.count_ones() {
-                2 => {
-                    one = pattern;
-                }
-                4 => {
-                    four = pattern;
-                }
-                _ => {}
-            }
-        }
-
-        let mut map = [0; 1 << 7];
-        for &pattern in possibilities {
-            map[pattern as usize] = match pattern.count_ones() {
-                2 => 1,
-                3 => 7,
-                4 => 4,
-                5 if (pattern & one).count_ones() == 2 => 3,
-                5 if (pattern & four).count_ones() == 2 => 2,
-                5 => 5,
-                6 if (pattern & !one).count_ones() == 5 => 6,
-                6 if (pattern & !four).count_ones() == 3 => 0,
-                6 => 9,
-                7 => 8,
-                _ => unreachable!(),
-            };
-        }
-        Self(map)
+    fn from(possibilities: &[Pattern]) -> Option<Self> {
+        let &one = possibilities
+            .iter()
+            .find(|&pattern| pattern.count_ones() == 2)?;
+        let &four = possibilities
+            .iter()
+            .find(|&pattern| pattern.count_ones() == 4)?;
+        Some(Self(one, four))
     }
 
     fn decode(&self, pattern: Pattern) -> u8 {
-        self.0[pattern as usize]
+        const PATTERN_LOOKUP: [u8; 16] = [4, 0, 1, 0, 0, 7, 9, 3, 0, 8, 0, 5, 0, 0, 6, 2];
+
+        PATTERN_LOOKUP[(self.0 ^ pattern).count_ones() as usize * 2
+            + (self.1 ^ pattern).count_ones() as usize * 2
+            - pattern.count_ones() as usize]
     }
 }
 
@@ -96,7 +75,7 @@ pub fn part2(input: &[Entry]) -> u32 {
     input
         .iter()
         .map(|Entry { signal, output }| {
-            let key = DecipherKey::from(signal);
+            let key = DecipherKey::from(signal).unwrap();
             output
                 .iter()
                 .map(|&x| key.decode(x) as u32)
