71279f87f28858c63f7668c17085ba962b7b9b10
Simon Gomizelj <simon@vodik.xyz>
2021-12-10 01:16:39 -0500

    Refactor day10
    No input is valid (as per puzzle instructions), so no need for the Ok
    case.

--- aoc-2021/src/day10.rs
+++ aoc-2021/src/day10.rs
@@ -2,48 +2,40 @@ pub fn parse_input(input: &str) -> Vec<&[u8]> {
     input.lines().map(|line| line.as_bytes()).collect()
 }
 
-enum ParseResult {
-    Ok,
+#[derive(Debug)]
+enum Expr {
+    Corrupt(u8),
     Incomplete(Vec<u8>),
-    UnexpectedToken(u8),
 }
 
-fn parse_expr(expr: &[u8]) -> ParseResult {
-    let mut stack = Vec::new();
-    for &c in expr {
-        match c {
-            b'(' | b'[' | b'{' | b'<' => stack.push(c),
-            b')' | b']' | b'}' | b'>' => match (stack.last(), c) {
-                (Some(b'('), b')')
-                | (Some(b'['), b']')
-                | (Some(b'{'), b'}')
-                | (Some(b'<'), b'>') => {
-                    stack.pop();
-                }
-                (_, c) => {
-                    return ParseResult::UnexpectedToken(c);
-                }
-            },
-            _ => unreachable!(),
+impl Expr {
+    fn parse(expr: &[u8]) -> Self {
+        let mut stack = Vec::new();
+        for &token in expr {
+            match token {
+                b'(' | b'[' | b'{' | b'<' => stack.push(token),
+                b')' | b']' | b'}' | b'>' => match (stack.pop(), token) {
+                    (Some(b'('), b')')
+                    | (Some(b'['), b']')
+                    | (Some(b'{'), b'}')
+                    | (Some(b'<'), b'>') => {}
+                    _ => return Expr::Corrupt(token),
+                },
+                _ => unreachable!(),
+            }
         }
-    }
-
-    if stack.is_empty() {
-        ParseResult::Ok
-    } else {
-        ParseResult::Incomplete(stack)
+        Expr::Incomplete(stack)
     }
 }
 
 pub fn part1(input: &[&[u8]]) -> usize {
     input
         .iter()
-        .map(|expr| parse_expr(expr))
-        .filter_map(|result| match result {
-            ParseResult::UnexpectedToken(b')') => Some(3),
-            ParseResult::UnexpectedToken(b']') => Some(57),
-            ParseResult::UnexpectedToken(b'}') => Some(1197),
-            ParseResult::UnexpectedToken(b'>') => Some(25137),
+        .filter_map(|expr| match Expr::parse(expr) {
+            Expr::Corrupt(b')') => Some(3),
+            Expr::Corrupt(b']') => Some(57),
+            Expr::Corrupt(b'}') => Some(1197),
+            Expr::Corrupt(b'>') => Some(25137),
             _ => None,
         })
         .sum()
@@ -52,22 +44,22 @@ pub fn part1(input: &[&[u8]]) -> usize {
 pub fn part2(input: &[&[u8]]) -> u64 {
     let mut count: Vec<u64> = input
         .iter()
-        .map(|expr| parse_expr(expr))
-        .filter_map(|result| match result {
-            ParseResult::Incomplete(stack) => Some(stack),
+        .filter_map(|expr| match Expr::parse(expr) {
+            Expr::Incomplete(stack) => Some(stack),
             _ => None,
         })
         .map(|stack| {
-            stack.into_iter().rev().fold(0, |acc, c| {
-                acc * 5
-                    + match c {
-                        b'(' => 1,
-                        b'[' => 2,
-                        b'{' => 3,
-                        b'<' => 4,
-                        _ => unreachable!(),
-                    }
-            })
+            stack
+                .into_iter()
+                .rev()
+                .map(|c| match c {
+                    b'(' => 1,
+                    b'[' => 2,
+                    b'{' => 3,
+                    b'<' => 4,
+                    _ => unreachable!(),
+                })
+                .fold(0, |acc, score| acc * 5 + score)
         })
         .collect();
 
