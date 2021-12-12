4159e4d0911304eea3572884ec513b1f9ab5cae3
Simon Gomizelj <simon@vodik.xyz>
2021-12-12 02:07:42 -0500

    Fix spelling on day12

--- aoc-2021/src/day12.rs
+++ aoc-2021/src/day12.rs
@@ -36,32 +36,32 @@ pub fn parse_input(input: &str) -> Vec<(Vertex, Vertex)> {
 
 #[derive(Debug)]
 pub struct Graph {
-    verticies: Vec<Vertex>,
+    vertices: Vec<Vertex>,
     edges: Vec<Vec<usize>>,
 }
 
 impl Graph {
     fn from(graph: &[(Vertex, Vertex)]) -> Self {
-        let mut verticies = Vec::new();
+        let mut vertices = Vec::new();
         for (a, b) in graph {
-            if !verticies.contains(a) {
-                verticies.push(a.clone());
+            if !vertices.contains(a) {
+                vertices.push(a.clone());
             }
-            if !verticies.contains(b) {
-                verticies.push(b.clone());
+            if !vertices.contains(b) {
+                vertices.push(b.clone());
             }
         }
 
-        let mut edges = vec![Vec::new(); verticies.len()];
+        let mut edges = vec![Vec::new(); vertices.len()];
         for (a, b) in graph {
-            let a_idx = verticies.iter().position(|v| v == a).unwrap();
-            let b_idx = verticies.iter().position(|v| v == b).unwrap();
+            let a_idx = vertices.iter().position(|v| v == a).unwrap();
+            let b_idx = vertices.iter().position(|v| v == b).unwrap();
 
             edges[a_idx].push(b_idx);
             edges[b_idx].push(a_idx);
         }
 
-        Self { verticies, edges }
+        Self { vertices, edges }
     }
 }
 
@@ -74,7 +74,7 @@ fn paths(graph: &Graph, vertex: usize, charge: bool) -> usize {
         .collect();
 
     while let Some((vertex, visited, charge)) = stack.pop() {
-        match &graph.verticies[vertex] {
+        match &graph.vertices[vertex] {
             Vertex::Start => {}
             Vertex::End => {
                 paths += 1;
@@ -115,7 +115,7 @@ pub fn part1(input: &[(Vertex, Vertex)]) -> usize {
     let graph = Graph::from(input);
 
     let start = graph
-        .verticies
+        .vertices
         .iter()
         .position(|v| matches!(v, Vertex::Start))
         .unwrap();
@@ -127,7 +127,7 @@ pub fn part2(input: &[(Vertex, Vertex)]) -> usize {
     let graph = Graph::from(input);
 
     let start = graph
-        .verticies
+        .vertices
         .iter()
         .position(|v| matches!(v, Vertex::Start))
         .unwrap();
