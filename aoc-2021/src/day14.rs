72990012a13fbea929f2f72e92bd6fe51f2b9938
Simon Gomizelj <simon@vodik.xyz>
2021-12-14 04:32:54 -0500

    Optimize day14 by more efficiently applying rules

--- aoc-2021/src/day14.rs
+++ aoc-2021/src/day14.rs
@@ -7,6 +7,14 @@ use nom::{
     Finish, IResult,
 };
 
+fn ord(c: u8) -> usize {
+    (c - b'A') as usize
+}
+
+fn encode(a: u8, b: u8) -> usize {
+    ord(a) * 26 + ord(b)
+}
+
 fn word(input: &str) -> IResult<&str, Vec<u8>> {
     map(
         take_while(move |c: char| c.is_ascii_uppercase()),
@@ -14,9 +22,9 @@ fn word(input: &str) -> IResult<&str, Vec<u8>> {
     )(input)
 }
 
-fn parse_rule(input: &str) -> IResult<&str, (usize, usize)> {
+fn parse_rule(input: &str) -> IResult<&str, (usize, usize, usize)> {
     map(separated_pair(word, tag(" -> "), word), |(pair, result)| {
-        (encode(pair[0], pair[1]), ord(result[0]))
+        (ord(pair[0]), ord(pair[1]), ord(result[0]))
     })(input)
 }
 
@@ -24,24 +32,18 @@ fn parse_initial(input: &str) -> IResult<&str, Vec<u8>> {
     word(input)
 }
 
-fn parse_rules(input: &str) -> IResult<&str, [usize; 26 * 26]> {
-    map(separated_list1(tag("\n"), parse_rule), |rules| {
-        let mut new_rules = [0usize; 26 * 26];
-        for (pair, result) in rules {
-            new_rules[pair] = result;
-        }
-        new_rules
-    })(input)
+fn parse_rules(input: &str) -> IResult<&str, Vec<(usize, usize, usize)>> {
+    separated_list1(tag("\n"), parse_rule)(input)
 }
 
-fn parse_file(input: &str) -> IResult<&str, (Vec<u8>, [usize; 26 * 26])> {
+fn parse_file(input: &str) -> IResult<&str, (Vec<u8>, Vec<(usize, usize, usize)>)> {
     terminated(
         separated_pair(parse_initial, tag("\n\n"), parse_rules),
         opt(tag("\n")),
     )(input)
 }
 
-pub fn parse_input(input: &str) -> (Vec<u8>, [usize; 26 * 26]) {
+pub fn parse_input(input: &str) -> (Vec<u8>, Vec<(usize, usize, usize)>) {
     match all_consuming(parse_file)(input).finish() {
         Ok((_, output)) => Ok(output),
         Err(Error { input, code }) => Err(Error {
@@ -52,15 +54,7 @@ pub fn parse_input(input: &str) -> (Vec<u8>, [usize; 26 * 26]) {
     .unwrap()
 }
 
-fn ord(c: u8) -> usize {
-    (c - b'A') as usize
-}
-
-fn encode(a: u8, b: u8) -> usize {
-    ord(a) * 26 + ord(b)
-}
-
-fn pairs(input: &[u8]) -> [usize; 26 * 26] {
+fn init_pairs(input: &[u8]) -> [usize; 26 * 26] {
     let mut pairs = [0; 26 * 26];
 
     for window in input.windows(2) {
@@ -71,30 +65,25 @@ fn pairs(input: &[u8]) -> [usize; 26 * 26] {
     pairs
 }
 
-fn step(pairs: &[usize], rules: &[usize]) -> [usize; 26 * 26] {
+fn step(pairs: &[usize], rules: &[(usize, usize, usize)]) -> [usize; 26 * 26] {
     let mut new_pairs = [0; 26 * 26];
 
-    for (pair, &count) in pairs.iter().enumerate() {
-        if count > 0 {
-            let mid = rules[pair];
-            let a = pair / 26;
-            let b = pair % 26;
-
-            new_pairs[a * 26 + mid] += count;
-            new_pairs[mid * 26 + b] += count;
-        }
+    for (a, b, c) in rules {
+        let count = pairs[a * 26 + b];
+        new_pairs[a * 26 + c] += count;
+        new_pairs[c * 26 + b] += count;
     }
 
     new_pairs
 }
 
-fn solve<const N: usize>(input: &[u8], rules: &[usize; 26 * 26]) -> usize {
+fn solve<const N: usize>(input: &[u8], rules: &[(usize, usize, usize)]) -> usize {
     let mut counts = [0usize; 26];
     counts[ord(input[0])] = 1;
 
     (0..N)
-        .fold(pairs(input), |pairs, _| step(&pairs, rules))
-        .into_iter()
+        .fold(init_pairs(input), |pairs, _| step(&pairs, rules))
+        .iter()
         .enumerate()
         .for_each(|(pair, count)| {
             counts[pair % 26] += count;
@@ -103,10 +92,10 @@ fn solve<const N: usize>(input: &[u8], rules: &[usize; 26 * 26]) -> usize {
     counts.iter().max().unwrap() - counts.iter().filter(|&&count| count > 0).min().unwrap()
 }
 
-pub fn part1(input: &(Vec<u8>, [usize; 26 * 26])) -> usize {
+pub fn part1(input: &(Vec<u8>, Vec<(usize, usize, usize)>)) -> usize {
     solve::<10>(&input.0, &input.1)
 }
 
-pub fn part2(input: &(Vec<u8>, [usize; 26 * 26])) -> usize {
+pub fn part2(input: &(Vec<u8>, Vec<(usize, usize, usize)>)) -> usize {
     solve::<40>(&input.0, &input.1)
 }
