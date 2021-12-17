aa05292b455f01d2f483e958f852c3bea00ed6af
Simon Gomizelj <simon@vodik.xyz>
2021-12-17 10:26:47 -0500

    Quick and dirty optimize day17 by pruning the trajectory simulation
    There's still a lot that can be done to speed this up

--- aoc-2021/src/day17.rs
+++ aoc-2021/src/day17.rs
@@ -6,27 +6,27 @@ use nom::{
     sequence::{preceded, separated_pair, terminated, tuple},
     Finish, IResult,
 };
-use std::{iter, ops::RangeInclusive, str::FromStr};
+use std::{ops::Range, str::FromStr};
 
 pub fn number<T: FromStr>(input: &str) -> IResult<&str, T> {
     map_res(recognize(tuple((opt(tag("-")), digit1))), FromStr::from_str)(input)
 }
 
-pub fn x_coord(input: &str) -> IResult<&str, RangeInclusive<i32>> {
+pub fn x_coord(input: &str) -> IResult<&str, Range<i32>> {
     map(
         preceded(tag("x="), separated_pair(number, tag(".."), number)),
-        |(a, b): (i32, i32)| i32::min(a, b)..=i32::max(a, b),
+        |(a, b): (i32, i32)| i32::min(a, b)..i32::max(a, b) + 1,
     )(input)
 }
 
-pub fn y_coord(input: &str) -> IResult<&str, RangeInclusive<i32>> {
+pub fn y_coord(input: &str) -> IResult<&str, Range<i32>> {
     map(
         preceded(tag("y="), separated_pair(number, tag(".."), number)),
-        |(a, b): (i32, i32)| i32::min(a, b)..=i32::max(a, b),
+        |(a, b): (i32, i32)| i32::min(a, b)..i32::max(a, b) + 1,
     )(input)
 }
 
-fn parse_rule(input: &str) -> IResult<&str, (RangeInclusive<i32>, RangeInclusive<i32>)> {
+fn parse_rule(input: &str) -> IResult<&str, (Range<i32>, Range<i32>)> {
     terminated(
         preceded(
             tag("target area: "),
@@ -36,7 +36,7 @@ fn parse_rule(input: &str) -> IResult<&str, (RangeInclusive<i32>, RangeInclusive
     )(input)
 }
 
-pub fn parse_input(input: &str) -> (RangeInclusive<i32>, RangeInclusive<i32>) {
+pub fn parse_input(input: &str) -> (Range<i32>, Range<i32>) {
     match all_consuming(parse_rule)(input).finish() {
         Ok((_, output)) => Ok(output),
         Err(Error { input, code }) => Err(Error {
@@ -50,49 +50,47 @@ pub fn parse_input(input: &str) -> (RangeInclusive<i32>, RangeInclusive<i32>) {
 #[derive(Debug, Clone, Copy)]
 struct Vector(i32, i32);
 
-impl Vector {
-    fn decay(&self) -> Self {
-        let x = self.0 - self.0.signum();
-        let y = self.1 - 1;
-        Vector(x, y)
-    }
-}
-
 #[derive(Debug, Clone, Copy)]
 struct Point(i32, i32);
 
-impl Point {
-    fn advance(&mut self, vec: &Vector) -> Self {
-        Self(self.0 + vec.0, self.1 + vec.1)
-    }
-}
-
-fn simulate(mut vec: Vector, bounds: &(RangeInclusive<i32>, RangeInclusive<i32>)) -> Option<i32> {
-    let mut point = Point(0, 0);
-
-    let path = iter::from_fn(move || {
-        point = point.advance(&vec);
-        vec = vec.decay();
-        Some(point)
-    })
-    .take(1_000);
+fn simulate(vec: Vector, bounds: &(Range<i32>, Range<i32>)) -> Option<i32> {
+    let trajectory = (1..vec.0 + 1)
+        .map(|n| {
+            let t = n * (n - 1) / 2;
+            Point(vec.0 * n - t, vec.1 * n - t)
+        })
+        .take_while(|&Point(x, _)| x < bounds.0.end);
 
     let mut max_y = 0;
-    for Point(x, y) in path {
+    for Point(x, y) in trajectory {
         max_y = max_y.max(y);
-
         if bounds.0.contains(&x) && bounds.1.contains(&y) {
             return Some(max_y);
-        } else if x > *bounds.0.end() || y < *bounds.1.start() {
-            break;
         }
     }
 
-    None
+    if !vec.1.is_positive() {
+        return None;
+    }
+
+    let max_x = vec.0 * (vec.0 + 1) / 2;
+    if !bounds.0.contains(&max_x) {
+        return None;
+    }
+
+    let max_y = vec.1 * (vec.1 + 1) / 2;
+    (1..)
+        .map(|n| {
+            let t = n * (n - 1) / 2;
+            max_y - t
+        })
+        .take_while(|&y| y >= bounds.1.start)
+        .any(|y| bounds.1.contains(&y))
+        .then(|| max_y)
 }
 
-pub fn part1(target: &(RangeInclusive<i32>, RangeInclusive<i32>)) -> i32 {
-    let t = *target.0.end() + 1;
+pub fn part1(target: &(Range<i32>, Range<i32>)) -> i32 {
+    let t = target.0.end;
 
     (0..t)
         .flat_map(|x| (-t..t).map(move |y| Vector(x, y)))
@@ -101,8 +99,8 @@ pub fn part1(target: &(RangeInclusive<i32>, RangeInclusive<i32>)) -> i32 {
         .unwrap()
 }
 
-pub fn part2(target: &(RangeInclusive<i32>, RangeInclusive<i32>)) -> usize {
-    let t = *target.0.end() + 1;
+pub fn part2(target: &(Range<i32>, Range<i32>)) -> usize {
+    let t = target.0.end;
 
     (0..t)
         .flat_map(|x| (-t..t).map(move |y| Vector(x, y)))
