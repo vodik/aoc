03a387db6d9ebdd4830ef692384aa2a0826154d8
Simon Gomizelj <simon@vodik.xyz>
2022-12-03 01:12:48 -0500

    Rename store to inventory

--- aoc-2022/src/day03.rs
+++ aoc-2022/src/day03.rs
@@ -1,12 +1,12 @@
-struct Store([bool; 52]);
+struct Inventory([bool; 52]);
 
-impl Store {
+impl Inventory {
     fn contains(&self, item: u8) -> bool {
         self.0[item as usize - 1]
     }
 }
 
-impl FromIterator<u8> for Store {
+impl FromIterator<u8> for Inventory {
     fn from_iter<T: IntoIterator<Item = u8>>(iter: T) -> Self {
         let mut store = [false; 52];
         for item in iter {
@@ -16,7 +16,7 @@ impl FromIterator<u8> for Store {
     }
 }
 
-pub fn item_priority(item: u8) -> u8 {
+fn item_priority(item: u8) -> u8 {
     match item {
         b'a'..=b'z' => item - b'a' + 1,
         b'A'..=b'Z' => item - b'A' + 27,
@@ -36,8 +36,11 @@ pub fn parse_input(input: &str) -> Vec<Vec<u8>> {
         .collect()
 }
 
-fn find_item(items: &[u8], store: &Store) -> u8 {
-    *items.iter().find(|&&item| store.contains(item)).unwrap()
+fn find_item(items: &[u8], inventory: &Inventory) -> u8 {
+    *items
+        .iter()
+        .find(|&&item| inventory.contains(item))
+        .unwrap()
 }
 
 pub fn part1(input: &[Vec<u8>]) -> u32 {
@@ -45,12 +48,12 @@ pub fn part1(input: &[Vec<u8>]) -> u32 {
         .iter()
         .map(|rucksack| {
             let (left, right) = rucksack.split_at(rucksack.len() / 2);
-            let store: Store = left.iter().copied().collect();
+            let inventory: Inventory = left.iter().copied().collect();
 
-            (right, store)
+            (right, inventory)
         })
-        .fold(0, |sum, (items, store)| {
-            sum + find_item(items, &store) as u32
+        .fold(0, |sum, (items, inventory)| {
+            sum + find_item(items, &inventory) as u32
         })
 }
 
@@ -58,15 +61,15 @@ pub fn part2(input: &[Vec<u8>]) -> u32 {
     input
         .chunks(3)
         .map(|rucksacks| {
-            let store: Store = rucksacks[0]
+            let inventory: Inventory = rucksacks[0]
                 .iter()
                 .chain(rucksacks[1].iter())
                 .copied()
                 .collect();
 
-            (&rucksacks[2], store)
+            (&rucksacks[2], inventory)
         })
-        .fold(0, |sum, (items, store)| {
-            sum + find_item(items, &store) as u32
+        .fold(0, |sum, (items, inventory)| {
+            sum + find_item(items, &inventory) as u32
         })
 }
