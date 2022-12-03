dc01d6ab471ee9f3bd2d797a29eb6530bbd2a276
Simon Gomizelj <simon@vodik.xyz>
2022-12-02 22:40:16 -0500

    Optimize day01 generator
    Cut down the number of allocations needed by summing immediately

--- aoc-2022/src/day01.rs
+++ aoc-2022/src/day01.rs
@@ -1,7 +1,7 @@
 use nom::{
     bytes::complete::tag,
     character::complete::digit1,
-    combinator::{all_consuming, map_res},
+    combinator::{all_consuming, map, map_res},
     error::Error,
     multi::separated_list1,
     sequence::terminated,
@@ -13,15 +13,15 @@ fn number<T: FromStr>(input: &str) -> IResult<&str, T> {
     map_res(digit1, FromStr::from_str)(input)
 }
 
-fn parse_group(input: &str) -> IResult<&str, Vec<u32>> {
-    separated_list1(tag("\n"), number)(input)
+fn parse_group(input: &str) -> IResult<&str, u32> {
+    map(separated_list1(tag("\n"), number), |nums| nums.iter().sum())(input)
 }
 
-fn parse_groups(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
+fn parse_groups(input: &str) -> IResult<&str, Vec<u32>> {
     separated_list1(tag("\n\n"), parse_group)(input)
 }
 
-pub fn parse_input(input: &str) -> Vec<Vec<u32>> {
+pub fn parse_input(input: &str) -> Vec<u32> {
     match all_consuming(terminated(parse_groups, tag("\n")))(input).finish() {
         Ok((_, output)) => Ok(output),
         Err(Error { input, code }) => Err(Error {
@@ -32,15 +32,15 @@ pub fn parse_input(input: &str) -> Vec<Vec<u32>> {
     .unwrap()
 }
 
-fn iter_packs(packs: &[Vec<u32>]) -> impl Iterator<Item = u32> + '_ {
-    packs.iter().map(|pack| pack.iter().sum())
+fn iter_packs(packs: &[u32]) -> impl Iterator<Item = u32> + '_ {
+    packs.iter().copied()
 }
 
-pub fn part1(input: &[Vec<u32>]) -> u32 {
+pub fn part1(input: &[u32]) -> u32 {
     iter_packs(input).max().unwrap()
 }
 
-pub fn part2(input: &[Vec<u32>]) -> u32 {
+pub fn part2(input: &[u32]) -> u32 {
     let top = iter_packs(input).fold([0; 3], |mut top, pack| {
         if pack > top[0] {
             top[0] = pack;
