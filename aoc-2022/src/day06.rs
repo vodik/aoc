e66e41dd5ec62abbc38fc69585d0b0329da49949
Simon Gomizelj <simon@vodik.xyz>
2022-12-06 18:40:54 -0500

    Optimize day06 solution with xor

--- aoc-2022/src/day06.rs
+++ aoc-2022/src/day06.rs
@@ -2,14 +2,20 @@ pub fn parse_input(input: &str) -> &[u8] {
     input.as_bytes()
 }
 
-fn ascii_alpha_bitmap(buf: &[u8]) -> u32 {
-    buf.iter().fold(0u32, |acc, &b| acc | 1 << (b - b'a'))
-}
+fn find_packet<const N: usize>(data: &[u8]) -> Option<usize> {
+    let buf: usize = data[..N].iter().fold(0, |acc, &b| acc ^ 1 << (b - b'a'));
+    if buf.count_ones() as usize == N {
+        return Some(N);
+    }
 
-fn find_packet<const N: usize>(buf: &[u8]) -> Option<usize> {
-    buf.windows(N as usize)
-        .position(|b| ascii_alpha_bitmap(b).count_ones() == N as u32)
-        .map(|start| start + N)
+    data.windows(N + 1)
+        .scan(buf, |buf, w| {
+            *buf ^= 1 << (w[N] - b'a');
+            *buf ^= 1 << (w[0] - b'a');
+            Some(buf.count_ones() as usize)
+        })
+        .position(|acc| acc == N)
+        .map(|pos| pos + N + 1)
 }
 
 pub fn part1(input: &[u8]) -> usize {
