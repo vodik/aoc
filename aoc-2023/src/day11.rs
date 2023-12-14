6052c6c04e1d0986143673411ca1b0042ce74636
Simon Gomizelj <simon@vodik.xyz>
2023-12-14 13:58:59 -0500

    Refactor day11

--- aoc-2023/src/day11.rs
+++ aoc-2023/src/day11.rs
@@ -19,8 +19,8 @@ pub fn parse_input(input: &str) -> Vec<Tile> {
         .collect()
 }
 
-fn pairs<T>(slice: &[T]) -> impl Iterator<Item = (&T, &T)> + '_ {
-    (0..slice.len()).flat_map(move |i| (i + 1..slice.len()).map(move |j| (&slice[i], &slice[j])))
+fn pairs<T: Copy>(slice: &[T]) -> impl Iterator<Item = (T, T)> + '_ {
+    (0..slice.len()).flat_map(move |i| (i + 1..slice.len()).map(move |j| (slice[i], slice[j])))
 }
 
 fn decompose(point: usize) -> (usize, usize) {
@@ -34,54 +34,46 @@ fn iter_galaxies(map: &[Tile]) -> impl Iterator<Item = usize> + '_ {
 }
 
 fn calculate_universe_expansion(map: &[Tile]) -> (Vec<usize>, Vec<usize>) {
-    let mut scale_x = Vec::with_capacity(10);
-    let mut scale_y = Vec::with_capacity(10);
-    for rank in 0..WIDTH {
-        if (0..WIDTH).all(|y| map[WIDTH * y + rank] == Tile::Empty) {
-            scale_x.push(rank);
-        }
-        if (0..WIDTH).all(|x| map[WIDTH * rank + x] == Tile::Empty) {
-            scale_y.push(rank);
-        }
-    }
+    let scale_x = (0..WIDTH)
+        .filter(|&x| (0..WIDTH).all(|y| map[WIDTH * y + x] == Tile::Empty))
+        .collect();
+    let scale_y = (0..WIDTH)
+        .filter(|&y| (0..WIDTH).all(|x| map[WIDTH * y + x] == Tile::Empty))
+        .collect();
     (scale_x, scale_y)
 }
 
-fn calculate_distance(
-    (&a, &b): (&usize, &usize),
+fn calculate_distance<const SCALE: usize>(
+    (a, b): (usize, usize),
     scale_x: &[usize],
     scale_y: &[usize],
-    scale: usize,
 ) -> usize {
-    let a = decompose(a);
-    let b = decompose(b);
+    let (ax, ay) = decompose(a);
+    let (bx, by) = decompose(b);
 
-    let min_x = a.0.min(b.0);
-    let max_x = a.0.max(b.0);
-    let min_y = a.1.min(b.1);
-    let max_y = a.1.max(b.1);
+    let (min_x, max_x) = (ax.min(bx), ax.max(bx));
+    let (min_y, max_y) = (ay.min(by), ay.max(by));
 
     let dx = max_x - min_x;
-    let dy = max_y - min_y;
-
     let x_expansion_factor = scale_x
         .iter()
-        .filter(|position| (min_x..max_x).contains(*position))
+        .filter(|&position| (min_x + 1..max_x).contains(position))
         .count();
 
+    let dy = max_y - min_y;
     let y_expansion_factor = scale_y
         .iter()
-        .filter(|position| (min_y..max_y).contains(*position))
+        .filter(|&position| (min_y + 1..max_y).contains(position))
         .count();
 
-    dx + dy + (x_expansion_factor + y_expansion_factor) * (scale - 1)
+    dx + dy + (x_expansion_factor + y_expansion_factor) * (SCALE - 1)
 }
 
 pub fn part1(map: &[Tile]) -> usize {
     let (scale_x, scale_y) = calculate_universe_expansion(map);
     let galaxies = iter_galaxies(map).collect::<Vec<_>>();
     pairs(&galaxies)
-        .map(|pair| calculate_distance(pair, &scale_x, &scale_y, 2))
+        .map(|pair| calculate_distance::<2>(pair, &scale_x, &scale_y))
         .sum()
 }
 
@@ -89,6 +81,6 @@ pub fn part2(map: &[Tile]) -> usize {
     let (scale_x, scale_y) = calculate_universe_expansion(map);
     let galaxies = iter_galaxies(map).collect::<Vec<_>>();
     pairs(&galaxies)
-        .map(|pair| calculate_distance(pair, &scale_x, &scale_y, 1_000_000))
+        .map(|pair| calculate_distance::<1_000_000>(pair, &scale_x, &scale_y))
         .sum()
 }
