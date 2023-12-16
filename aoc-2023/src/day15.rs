f163978d0fa2091dc12832ad9bd1c3062901baa5
Simon Gomizelj <simon@vodik.xyz>
2023-12-16 14:38:02 -0500

    Optimize day15

--- aoc-2023/src/day15.rs
+++ aoc-2023/src/day15.rs
@@ -1,7 +1,12 @@
+use std::{
+    mem::{self, MaybeUninit},
+    ptr,
+};
+
 pub fn parse_input(input: &str) -> Vec<Vec<u8>> {
     input
         .trim_end()
-        .split(",")
+        .split(',')
         .map(|s| s.bytes().collect::<Vec<_>>())
         .collect()
 }
@@ -16,60 +21,90 @@ pub fn part1(input: &[Vec<u8>]) -> u32 {
     input.iter().map(|step| hash(step) as u32).sum()
 }
 
+fn hash_and_label(input: &[u8]) -> (u8, u32) {
+    input.iter().fold((0u8, 0u32), |(hash, label), &c| {
+        (
+            hash.wrapping_add(c).wrapping_mul(17),
+            (label << 8) | c as u32,
+        )
+    })
+}
+
+#[derive(Debug, Default, Clone)]
+struct Entry {
+    lenses: [Option<(u32, u8)>; 6],
+    len: usize,
+}
+
+impl Entry {
+    fn remove(&mut self, label: u32) {
+        let position = self.iter().position(|entry| entry.0 == label);
+        if let Some(position) = position {
+            self.lenses.copy_within(position + 1.., position);
+            self.len -= 1;
+        }
+    }
+
+    fn insert(&mut self, label: u32, focal_length: u8) {
+        if let Some(entry) = self.iter_mut().find(|entry| entry.0 == label) {
+            entry.1 = focal_length;
+            return;
+        }
+        self.lenses[self.len] = Some((label, focal_length));
+        self.len += 1;
+    }
+
+    fn len(&self) -> usize {
+        self.len
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &(u32, u8)> + '_ {
+        self.lenses.iter().take(self.len).flatten()
+    }
+
+    fn iter_mut(&mut self) -> impl Iterator<Item = &mut (u32, u8)> + '_ {
+        self.lenses.iter_mut().take(self.len).flatten()
+    }
+}
+
 pub fn part2(input: &[Vec<u8>]) -> usize {
-    let mut boxes = vec![vec![]; 256];
+    let mut boxes = stack_alloc_boxes();
 
     for step in input {
         let len = step.len();
         match step[len - 2..] {
             [_, b'-'] => {
-                let label = &step[..len - 1];
-
-                boxes
-                    .get_mut(hash(label) as usize)
-                    .unwrap()
-                    .retain(|&(key, _)| key != label);
+                let (hash, label) = hash_and_label(&step[..len - 1]);
+                boxes.get_mut(hash as usize).unwrap().remove(label);
             }
             [b'=', digit] => {
-                let label = &step[..len - 2];
-                let focal_length = (digit as char).to_digit(10).unwrap() as usize;
-
-                let entry = boxes.get_mut(hash(label) as usize).unwrap();
-                match entry.iter_mut().find(|(key, _)| key == &label) {
-                    Some(slot) => {
-                        (*slot).1 = focal_length;
-                    }
-                    None => {
-                        entry.push((label, focal_length));
-                    }
-                }
+                let (hash, label) = hash_and_label(&step[..len - 2]);
+                boxes
+                    .get_mut(hash as usize)
+                    .unwrap()
+                    .insert(label, digit - b'0');
             }
-            _ => unreachable!(),
+            _ => {}
         }
     }
 
     boxes
         .iter()
-        .enumerate()
-        .map(|(box_no, entry)| {
+        .zip(1..boxes.len() + 1)
+        .map(|(entry, box_no)| {
             entry
                 .iter()
-                .enumerate()
-                .map(|(slot_no, &(_, focal_length))| (box_no + 1) * (slot_no + 1) * focal_length)
+                .zip(1..entry.len() + 1)
+                .map(|(&(_, focal_length), lens_pos)| box_no * lens_pos * focal_length as usize)
                 .sum::<usize>()
         })
         .sum()
 }
 
-fn fun_name<'a>() -> [Vec<(&'a [u8], usize)>; 256] {
-    let mut data: [std::mem::MaybeUninit<Vec<(&[u8], usize)>>; 256] =
-        unsafe { std::mem::MaybeUninit::uninit().assume_init() };
-
-    for elem in &mut data[..] {
-        unsafe {
-            std::ptr::write(elem.as_mut_ptr(), Vec::with_capacity(4));
-        }
-    }
-
-    unsafe { std::mem::transmute::<_, [Vec<(&[u8], usize)>; 256]>(data) }
+fn stack_alloc_boxes() -> [Entry; 256] {
+    let mut data: [MaybeUninit<Entry>; 256] = unsafe { MaybeUninit::uninit().assume_init() };
+    data.iter_mut().for_each(|elem| unsafe {
+        ptr::write(elem.as_mut_ptr(), Entry::default());
+    });
+    unsafe { mem::transmute::<_, [Entry; 256]>(data) }
 }
