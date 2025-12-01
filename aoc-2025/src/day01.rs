5066791bd74bd1a081c44f9c51277fbcaf6fa5bb
Simon Gomizelj <simon@vodik.xyz>
2025-12-01 10:51:32 -0500

    Slightly cleanup day01

--- aoc-2025/src/day01.rs
+++ aoc-2025/src/day01.rs
@@ -1,15 +1,18 @@
 #[derive(Debug, Copy, Clone)]
-pub enum Op {
-    Left(i32),
-    Right(i32),
+pub enum Dir {
+    Left,
+    Right,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Op {
+    dir: Dir,
+    step: i32,
 }
 
 impl Op {
-    fn step(&self) -> i32 {
-        match *self {
-            Op::Left(step) => step,
-            Op::Right(step) => step,
-        }
+    pub fn new(dir: Dir, step: i32) -> Self {
+        Self { dir, step }
     }
 }
 
@@ -20,8 +23,8 @@ pub fn parse_input(input: &str) -> Vec<Op> {
             let (dir, step) = line.split_at(1);
             let step = step.parse::<i32>().unwrap();
             match dir {
-                "L" => Op::Left(-step),
-                "R" => Op::Right(step),
+                "L" => Op::new(Dir::Left, -step),
+                "R" => Op::new(Dir::Right, step),
                 _ => unreachable!(),
             }
         })
@@ -31,7 +34,7 @@ pub fn parse_input(input: &str) -> Vec<Op> {
 pub fn part1(ops: &[Op]) -> u32 {
     ops.iter()
         .scan(50, |pos, op| {
-            *pos = (*pos + op.step()).rem_euclid(100);
+            *pos = (*pos + op.step).rem_euclid(100);
             Some((*pos == 0) as u32)
         })
         .sum()
@@ -40,15 +43,15 @@ pub fn part1(ops: &[Op]) -> u32 {
 pub fn part2(ops: &[Op]) -> u32 {
     ops.iter()
         .scan(50i32, |pos, op| {
-            let residue = match op {
-                Op::Left(_) => *pos,
-                Op::Right(_) => (-*pos).rem_euclid(100),
+            let residue = match op.dir {
+                Dir::Left => *pos,
+                Dir::Right => (-*pos).rem_euclid(100),
             };
 
-            *pos = (*pos + op.step()).rem_euclid(100);
+            *pos = (*pos + op.step).rem_euclid(100);
 
             let first_zero_step = ((residue + 99) % 100) + 1;
-            let overshoot = op.step().abs() - first_zero_step;
+            let overshoot = op.step.abs() - first_zero_step;
             let mask = (overshoot >= 0) as u32;
             Some(mask * (1 + overshoot as u32 / 100))
         })
