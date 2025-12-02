3814c9943265ab3d94d8f4fd3771a57a9d7be5a6
Simon Gomizelj <simon@vodik.xyz>
2025-12-02 00:55:47 -0500

    Optimize day02 with a custom buffer
    Write digits backwards manually - faster to do and doesn't matter for
    the solution!

--- aoc-2025/src/day02.rs
+++ aoc-2025/src/day02.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::ops::Index;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Range {
@@ -6,12 +6,43 @@ pub struct Range {
     end: u64,
 }
 
+#[derive(Default)]
+struct Buffer([u8; 20]);
+
+impl Buffer {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn set_value(&mut self, mut n: u64) -> usize {
+        let mut i = 0;
+        loop {
+            self.0[i] = (n % 10) as u8;
+            n /= 10;
+            i += 1;
+            if n == 0 {
+                break;
+            }
+        }
+        i
+    }
+}
+
+impl Index<usize> for Buffer {
+    type Output = u8;
+
+    #[inline(always)]
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
 pub fn parse_input(input: &str) -> Vec<Range> {
     input
         .trim()
-        .split(",")
+        .split(',')
         .map(|line| {
-            let (start, end) = line.split_once("-").unwrap();
+            let (start, end) = line.split_once('-').unwrap();
             let start = start.parse::<u64>().unwrap();
             let end = end.parse::<u64>().unwrap();
             Range { start, end }
@@ -19,57 +50,40 @@ pub fn parse_input(input: &str) -> Vec<Range> {
         .collect()
 }
 
-fn sequence_repeated_twice(num: u64, buf: &mut Vec<u8>) -> bool {
-    buf.clear();
-    write!(buf, "{num}").unwrap();
-
-    if buf.len().is_multiple_of(2) {
-        let mut iter = buf.chunks(2);
-        if let Some(first) = iter.next()
-            && iter.all(|element| element == first)
-        {
-            return true;
-        }
+fn sequence_repeated_twice(num: u64, buf: &mut Buffer) -> bool {
+    let len = buf.set_value(num);
+    if !len.is_multiple_of(2) {
+        return false;
     }
 
-    false
+    let mid = len / 2;
+    (0..mid).all(|i| buf[i] == buf[i + mid])
 }
 
-fn sequence_repeated(num: u64, buf: &mut Vec<u8>) -> bool {
-    buf.clear();
-    write!(buf, "{num}").unwrap();
+fn sequence_repeated(num: u64, buf: &mut Buffer) -> bool {
+    let len = buf.set_value(num);
 
-    for i in 1..buf.len() {
-        if !buf.len().is_multiple_of(i) {
-            continue;
-        }
-
-        let mut iter = buf.chunks(i);
-        if let Some(first) = iter.next()
-            && iter.all(|element| element == first)
-        {
-            return true;
-        }
-    }
-    false
+    (1..=len / 2)
+        .filter(|&k| len.is_multiple_of(k))
+        .any(|k| (k..len).all(|i| buf[i] == buf[i % k]))
 }
 
 pub fn part1(ranges: &[Range]) -> u64 {
-    let mut buf = Vec::with_capacity(16);
+    let mut buf = Buffer::new();
 
     ranges
         .iter()
-        .flat_map(|range| range.start..(range.end + 1))
+        .flat_map(|range| range.start..=range.end)
         .filter(|&id| sequence_repeated_twice(id, &mut buf))
         .sum()
 }
 
 pub fn part2(ranges: &[Range]) -> u64 {
-    let mut buf = Vec::with_capacity(16);
+    let mut buf = Buffer::new();
 
     ranges
         .iter()
-        .flat_map(|range| range.start..(range.end + 1))
+        .flat_map(|range| range.start..=range.end)
         .filter(|&id| sequence_repeated(id, &mut buf))
         .sum()
 }
